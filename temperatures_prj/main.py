24ebe57bbebb51c7f146b3160b7b706ff5354a06
Alina <alinazolotopup99@gmail.com>
2026-05-22 20:56:08 +0200

    add library colorama

---
+++ temperatures_prj/main.py
@@ -0,0 +1,19 @@
+from data import load_data, clean_data
+from processing import calculate_statistics
+
+def main():
+    filename = "./temperatures_prj/temperatures.txt"
+    raw_data = load_data(filename)
+    temperatures = clean_data(raw_data)
+    stats = calculate_statistics(temperatures)
+
+    if stats:
+        print(f"Minimum Temperature: {stats['min']}°C")
+        print(f"Maximum Temperature: {stats['max']}°C")
+        print(f"Average Temperature: {stats['average']:.2f}°C")
+        print(f"Median Temperature: {stats['median']:.2f}°C")
+    else:
+        print("No temperature data available.")
+
+if __name__ == "__main__":
+    main()
